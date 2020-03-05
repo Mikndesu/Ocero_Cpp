aa6913bdbeef2518f3ccd5937e66e99c53b18e7d
Mitsuki Goto <minionnzu20041125@yahoo.co.jp>
2020-03-05 15:20:13 +0900

    Intial Commit

---
+++ Ocero/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Ocero
+//
+//  Created by MitsukiGoto on 2020/03/05.
+//  Copyright © 2020 MitsukiGoto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    return 0;
+}
